fc165cdc0c3e608051182ad33225582bb6e7d0df
lopezrj <lopezrj@hotmail.com>
2020-07-30 15:00:30 -0600

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawerRight"
-      app
-      clipped
-      right
-    >
+    <v-navigation-drawer v-model="drawerRight" app clipped right>
       <v-list dense>
         <v-list-item @click.stop="right = !right">
           <v-list-item-action>
@@ -18,23 +13,16 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      clipped-right
-      color="blue-grey"
-      dark
-    >
+    <v-app-bar app clipped-right color="blue-grey" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Toolbar</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        @click.stop="drawerRight = !drawerRight"
+      ></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-
+    <v-navigation-drawer v-model="drawer" app>
       <LeftBar />
       <v-list dense>
         <v-list-item @click.stop="left = !left">
@@ -48,31 +36,15 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-navigation-drawer
-      v-model="left"
-      fixed
-      temporary
-    ></v-navigation-drawer>
+    <v-navigation-drawer v-model="left" fixed temporary></v-navigation-drawer>
 
     <v-main>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          justify="center"
-          align="center"
-        >
+      <v-container class="fill-height" fluid>
+        <v-row justify="center" align="center">
           <v-col class="shrink">
             <v-tooltip right>
               <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
+                <v-btn :href="source" icon large target="_blank" v-on="on">
                   <v-icon large>mdi-code-tags</v-icon>
                 </v-btn>
               </template>
@@ -90,11 +62,7 @@
       temporary
     ></v-navigation-drawer>
 
-    <v-footer
-      app
-      color="blue-grey"
-      class="white--text"
-    >
+    <v-footer app color="blue-grey" class="white--text">
       <span>Vuetify</span>
       <v-spacer></v-spacer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -103,19 +71,19 @@
 </template>
 
 <script>
-  import LeftBar from './components/LeftBar'
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-      drawerRight: null,
-      right: false,
-      left: false,
-    }),
-    components:{
-        LeftBar
-     },
+import LeftBar from "./components/LeftBar";
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null,
+    drawerRight: null,
+    right: false,
+    left: false
+  }),
+  components: {
+    LeftBar
   }
+};
 </script>
